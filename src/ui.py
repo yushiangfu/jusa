71fe1714ad6d0a905cc5a420f34bf00344306826
bob.fu <yushiangfu@gmail.com>
2015-11-20 22:22:53 +0800

    fix pylint for src/ui.py

--- src/ui.py
+++ src/ui.py
@@ -7,57 +7,16 @@ from tkinter import ttk
 from PIL import Image, ImageTk
 import stockfilter
 
-class App(tkinter.Frame):
-    """
-    ui implementation
-    """
+# widgets@tkinter are highly configurable while ttk provides themed widgets
+# so, use ttk if possible
+class App():
+    """ ui implementation """
     def __init__(self, parent):
-        tkinter.Frame.__init__(self, parent)
         self.parent = parent
-        self.filesetting = None
-        self.run = None
-        self.tabs = None
-        self.filters_table = None
-        self.filters_params = None
-        self.init_ui()
-
-    def init_ui(self):
-        """
-        init each sub ui
-        """
-        self.parent.title('Jusa - Stock Analyzer')
-
-        self.init_filter_settings_ui()
-        self.tabs.grid(row=0, column=0)
-        self.parent.grid_columnconfigure(0, weight=1)
-
-    def init_filters_ui(self, parent):
-        """
-        according filters_table to init widgets of each filter
-        """
-        def configure_interior(event):
-            """
-            callback for scrollbar
-            """
-            _ = event
-            cnvs.configure(scrollregion=cnvs.bbox("all"))
-
-        tmp = tkinter.Scrollbar(parent, orient="vertical")
-        # sticky is used to expand scrollbar vertically
-        tmp.grid(row=0, column=1, sticky='ns')
-
-        cnvs = tkinter.Canvas(parent, yscrollcommand=tmp.set)
-        cnvs.grid(row=0, column=0)
-        parent.grid_columnconfigure(0, weight=1)
-        tmp.config(command=cnvs.yview)
-
-        interior = tkinter.Frame(cnvs)
-        # anchor can reset scrollbar to top
-        cnvs.create_window(0, 0, window=interior, anchor='nw')
-        interior.bind("<Configure>", configure_interior)
-
-
-        self.filters_table = [
+        self.stocks_tab = None
+        self.date_tab = None
+        self.filters_tab = None
+        self.filters_descs = [
             [stockfilter.new_price_in_n_days, 'c', '股價創', 'e', '日新', 'e'],
             [stockfilter.new_revenue_in_n_months, 'c', '營收創', 'e', '月新', 'e'],
             [None, 'c', '最近', 'e', '年, 毛利率不曾低於', 'e', '%'],
@@ -65,163 +24,244 @@ class App(tkinter.Frame):
             [None, 'c', '毛利率創歷史新高'],
             [None, 'c', '連續', 'e', '年發放現金股利'],
             ]
-#parent.filters_table = filters_table
-        self.filters_params = []
-        for cond in self.filters_table:
-            frm = tkinter.Frame(interior)
-            frm.grid(sticky='w')
-            col = 0
-            widget_set = []
-            for widget in cond[1:]:
-                if widget == 'c':
-                    var = tkinter.IntVar()
-                    tmp = tkinter.Checkbutton(frm, variable=var)
-                    tmp.grid(row=0, column=col)
-                    widget_set.append(var)
-                elif widget == 'e':
-                    var = tkinter.StringVar()
-                    tmp = tkinter.Entry(frm, width=3, textvariable=var)
-                    tmp.grid(row=0, column=col)
-                    widget_set.append(tmp)
-                else:
-                    lbl = tkinter.Label(frm, text=widget, font='purisa 10')
-                    lbl.grid(row=0, column=col)
-                col += 1
-            self.filters_params.append(widget_set)
+        self.filters_vars = None
+
+        self.init_ui()
 
 
-    def init_single_or_specified_ui(self, parent, single_name, \
-            specified_hint):
-        """
-        used to init stock/filter tab
-        which contains 'default'/'specified' options
-        """
+    def init_ui(self):
+        """ init each sub ui """
+        # set main ui's size/pos/title
+        app_width = 800
+        app_height = 800
+        pos_x = (self.parent.winfo_screenwidth() - app_width) // 2
+        pos_y = (self.parent.winfo_screenheight() - app_height) // 2
+        self.parent.geometry('%dx%d+%d+%d' % (app_width, app_height, pos_x, pos_y))
+        self.parent.title('Jusa - Stock Analyzer')
 
-        def select():
-            """ callback of radio button """
-            sel = rvar.get()
-            if sel == 'Specified':
-                ent1.configure(state='normal')
-                ent1.select_range(0, tkinter.END)
-                ent1.focus()
-            else:
-                ent1.configure(state='disabled')
+        filter_settings = self.init_filter_settings_ui()
+        filter_settings.grid(row=0, column=0)
 
-        # init radio buttons for 'all' and 'specified'
-        rvar = tkinter.StringVar()
-        rb0 = tkinter.Radiobutton(parent, font='purisa 10', \
-                variable=rvar, command=select, \
-                value=single_name, text=single_name)
-        rb0.select()
-        rb1 = tkinter.Radiobutton(parent, font='purisa 10', \
-                variable=rvar, command=select, \
-                value=u'Specified', text='Specified')
-        rb0.grid(row=0, column=0, sticky='W')
-        rb1.grid(row=1, column=0, sticky='W')
-        parent.grid_columnconfigure(0, pad=10)
+        self.parent.grid_columnconfigure(0, weight=1)
 
-        # init entry for 'specified'
-        evar = tkinter.StringVar()
-        evar.set(specified_hint)
-        ent1 = tkinter.Entry(parent, state=tkinter.DISABLED, \
-                textvariable=evar, width=50, font='purisa 10')
-        ent1.grid(row=1, column=1)
-        parent.e1 = ent1
-        parent.rvar = rvar
-
-
-
-
+        # FIXME: add 'result' ui
 
 
     def init_filter_settings_ui(self):
         """
-        the main ui, which is composed of stock/date/filter tabs
+        the 'filter settings' ui, which is composed of stock/date/filter tabs
         """
-        self.filesetting = \
-            tkinter.LabelFrame(self.parent, text='Filter Settings', \
-            font='purisa 10', padx=10, pady=10)
-        self.filesetting.grid(row=0, column=0)
+        # 'filter settings' label frame
+        filter_settings = \
+            tkinter.LabelFrame(self.parent, text='Filter Settings',
+                               font='purisa 10', padx=10, pady=10) # internal padding
+
+        # 'setting' tabs
+        tabs = self.init_tabs_ui(filter_settings)
+        tabs.grid(row=0, column=0)
+
+        # 'run' button
+        run_button = \
+            tkinter.Button(filter_settings, text='Run',
+                           command=self.bypass_params, font='purisa 10')
+        run_button.grid(row=0, column=1, sticky='N')
+
+        return filter_settings
+
+
+    def init_tabs_ui(self, parent):
+        """ init each tab """
+        tabs = ttk.Notebook(parent)
+        tabs.grid()
 
         # tab for 'stocks'
-        self.tabs = ttk.Notebook(self.filesetting)
-        fr0 = ttk.Frame(self.tabs, padding=10)
-        fr0.img = Image.open("res/target.png")
-        fr0.pimg = ImageTk.PhotoImage(fr0.img)
-        self.filesetting.stocks_tab = fr0
-        self.init_single_or_specified_ui(fr0, 'All', 'e.g. 1234,2345,3456')
+        self.stocks_tab = ttk.Frame(tabs, padding=10)
+        tmp_img = Image.open("res/target.png")
+        self.stocks_tab.pimg = ImageTk.PhotoImage(tmp_img) # pimg must be referenced
+        tabs.add(self.stocks_tab, image=self.stocks_tab.pimg)
+        self.init_stocks_ui()
 
         # tab for 'date'
-        fr1 = ttk.Frame(self.tabs, padding=10)
-        fr1.img = Image.open("res/date.jpg")
-        fr1.pimg = ImageTk.PhotoImage(fr1.img)
-        self.filesetting.date_tab = fr1
-        self.init_single_or_specified_ui(fr1, 'Today', \
-            'e.g. 2015/10/1 or 2015/10/1-2015/11/1')
+        self.date_tab = ttk.Frame(tabs, padding=10)
+        tmp_img = Image.open("res/date.jpg")
+        self.date_tab.pimg = ImageTk.PhotoImage(tmp_img)
+        tabs.add(self.date_tab, image=self.date_tab.pimg)
+        self.init_date_ui()
 
         # tab for 'filters'
-        fr2 = ttk.Frame(self.tabs, padding=10)
-        fr2.img = Image.open("res/filter.jpg")
-        fr2.pimg = ImageTk.PhotoImage(fr2.img)
-        self.filesetting.filters_tab = fr2
-        fr1.pimg = ImageTk.PhotoImage(fr1.img)
-        self.init_filters_ui(fr2)
+        self.filters_tab = ttk.Frame(tabs, padding=10)
+        tmp_img = Image.open("res/filter.jpg")
+        self.filters_tab.pimg = ImageTk.PhotoImage(tmp_img)
+        tabs.add(self.filters_tab, image=self.filters_tab.pimg)
+        self.init_filters_ui()
 
-        self.tabs.add(fr0, image=fr0.pimg)
-        self.tabs.add(fr1, image=fr1.pimg)
-        self.tabs.add(fr2, image=fr2.pimg)
+        return tabs
 
-        self.filesetting.grid_columnconfigure(0, pad=10)
 
-        self.run = tkinter.Button(self.filesetting, text='Run', \
-            command=self.run_filters, font='purisa 10')
-        self.run.grid(row=0, column=1, sticky='N')
+    def init_stocks_ui(self):
+        """ init stocks tab """
+        def select():
+            """ callback of radio button """
+            sel = rb_var.get()
+            if sel == 'Specified':
+                ent_specified.configure(state='normal')
+                ent_specified.select_range(0, tkinter.END)
+                ent_specified.focus()
+            else:
+                ent_specified.configure(state='disabled')
 
-    def run_filters(self):
-        """
-        get params from widget and bypass to filters
-        """
-        if self.filesetting.stocks_tab.rvar.get() == 'All':
+        rb_var = tkinter.StringVar()
+        self.stocks_tab.rb_var = rb_var
+
+        # 'default' button
+        rb_default = \
+            tkinter.Radiobutton(self.stocks_tab, font='purisa 10',
+                                variable=rb_var, command=select,
+                                value='All', text='All')
+        rb_default.grid(row=0, column=0, sticky='W')
+        rb_default.select()
+
+        # 'specified' button
+        rb_specified = \
+            tkinter.Radiobutton(self.stocks_tab, font='purisa 10',
+                                variable=rb_var, command=select,
+                                value='Specified', text='Specified')
+        rb_specified.grid(row=1, column=0, sticky='W')
+
+        # make space between ratio button and entry
+        self.stocks_tab.grid_columnconfigure(0, pad=10)
+
+        # 'specified' entry
+        ent_var = tkinter.StringVar()
+        ent_var.set('e.g. 1234,2345,3456')
+        ent_specified = \
+            tkinter.Entry(self.stocks_tab, state='disabled',
+                          textvariable=ent_var, width=50, font='purisa 10')
+        ent_specified.grid(row=1, column=1)
+        self.stocks_tab.ent_specified = ent_specified
+
+
+    def init_date_ui(self):
+        """ init stocks tab """
+        def select():
+            """ callback of radio button """
+            sel = rb_var.get()
+            if sel == 'Specified':
+                ent_specified.configure(state='normal')
+                ent_specified.select_range(0, tkinter.END)
+                ent_specified.focus()
+            else:
+                ent_specified.configure(state='disabled')
+
+        rb_var = tkinter.StringVar()
+        self.date_tab.rb_var = rb_var
+
+        # 'default' button
+        rb_default = \
+            tkinter.Radiobutton(self.date_tab, font='purisa 10',
+                                variable=rb_var, command=select,
+                                value='Today', text='Today')
+        rb_default.grid(row=0, column=0, sticky='W')
+        rb_default.select()
+
+        # 'specified' button
+        rb_specified = \
+            tkinter.Radiobutton(self.date_tab, font='purisa 10',
+                                variable=rb_var, command=select,
+                                value='Specified', text='Specified')
+        rb_specified.grid(row=1, column=0, sticky='W')
+
+        # make space between ratio button and entry
+        self.date_tab.grid_columnconfigure(0, pad=10)
+
+        # 'specified' entry
+        ent_var = tkinter.StringVar()
+        ent_var.set('e.g. 2015/10/1 or 2015/10/1-2015/11/1')
+        ent_specified = \
+            tkinter.Entry(self.date_tab, state='disabled',
+                          textvariable=ent_var, width=50, font='purisa 10')
+        ent_specified.grid(row=1, column=1)
+        self.date_tab.ent_specified = ent_specified
+
+
+    def init_filters_ui(self):
+        """ according filters_descs to init widgets of each filter """
+        def configure_interior(event):
+            """ callback for scrollbar """
+            _ = event
+            canvas.configure(scrollregion=canvas.bbox("all"))
+
+        # scrollbar
+        scrollbar = tkinter.Scrollbar(self.filters_tab, orient="vertical")
+        scrollbar.grid(row=0, column=1, sticky='ns') # use sticky to expand
+
+        # canvas
+        canvas = tkinter.Canvas(self.filters_tab, yscrollcommand=scrollbar.set)
+        canvas.grid(row=0, column=0)
+
+        # bind canvas with scrollbar
+        scrollbar.config(command=canvas.yview)
+
+        self.filters_tab.grid_columnconfigure(0, weight=1)
+
+        # interior frame
+        interior = tkinter.Frame(canvas)
+        # anchor can reset scrollbar to top
+        canvas.create_window(0, 0, window=interior, anchor='nw')
+        interior.bind("<Configure>", configure_interior)
+
+
+        self.filters_vars = []
+        for filt in self.filters_descs:
+            frm = tkinter.Frame(interior)
+            frm.grid(sticky='w')
+            col = 0
+            filter_vars = []
+            for desc in filt[1:]: # manually avoid filter function
+                if desc == 'c':
+                    var = tkinter.IntVar()
+                    widget = tkinter.Checkbutton(frm, variable=var)
+                    filter_vars.append(var)
+                elif desc == 'e':
+                    var = tkinter.StringVar()
+                    widget = tkinter.Entry(frm, width=4, textvariable=var)
+                    filter_vars.append(widget)
+                else:
+                    widget = tkinter.Label(frm, text=desc, font='purisa 10')
+                widget.grid(row=0, column=col)
+                col += 1
+            self.filters_vars.append(filter_vars)
+
+
+    def bypass_params(self):
+        """ get params from widget and bypass to filters """
+        if self.stocks_tab.rb_var.get() == 'All':
             stocks = 'all'
         else:
-            stocks = self.filesetting.stocks_tab.e1.get()
+            stocks = self.stocks_tab.ent_specified.get()
 
-        if self.filesetting.date_tab.rvar.get() == 'Today':
+        if self.date_tab.rb_var.get() == 'Today':
             date = 'today'
         else:
-            date = self.filesetting.date_tab.e1.get()
+            date = self.date_tab.ent_specified.get()
 
-        i = 0
-        filters = []
-        for cond in self.filters_params:
-            if int(cond[0].get()): # [1] for checkbutton
-                #filt = self.filesetting.filters_tab.filters_table[i][0]
-                filt = self.filters_table[i][0]
-                ft_params = [widget.get() for widget in cond[1:]]
-                filters.append([filt, ft_params])
-            i += 1
-            #for widget in cond:
-                #print(widget.get())
-        stockfilter.process_filter(stocks, date, filters)
+        filters_params = []
+        for i in range(len(self.filters_vars)):
+            filter_vars = self.filters_vars[i]
+            if int(filter_vars[0].get()): # [0] for checkbutton
+                filt = self.filters_descs[i][0]
+                filter_params = [var.get() for var in filter_vars[1:]]
+                filters_params.append([filt] + filter_params)
+        stockfilter.process_filters(stocks, date, filters_params)
 
 
-
-
-
-
-APP_WIDTH = 700
-APP_HEIGHT = 700
 def main():
     """
     application entry point
     """
     root = tkinter.Tk()
-    pos_x = (root.winfo_screenwidth() - APP_WIDTH) // 2
-    pos_y = (root.winfo_screenheight() - APP_HEIGHT) // 2
-    root.geometry('%dx%d+%d+%d' % (APP_WIDTH, APP_HEIGHT, pos_x, pos_y))
-    app = App(root)
-    app.mainloop()
-
+    App(root)
+    root.mainloop()
 
 
 if __name__ == '__main__':
